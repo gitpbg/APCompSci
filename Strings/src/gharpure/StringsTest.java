71e326347f86103b4f785dc4d3ff214394144a2a
gharpure <prasad@gharpure.com>
2016-12-16 18:26:47 -0800

    Initial Checkin for Strings project

---
+++ Strings/src/gharpure/StringsTest.java
@@ -0,0 +1,89 @@
+package gharpure;
+
+/**
+ * Created by gharpure on 12/16/16.
+ * Intended to be coaching for the String class
+ */
+public class StringsTest {
+
+    /**
+     * This function will accept the given string and print out the count for each character from A-Z
+     * e.g. for the string "Hello"
+     * the output should look like:
+     * A => 0
+     * B => 0
+     * C => 0
+     * D => 0
+     * ...
+     * H => 1
+     * ...
+     * Z => 0
+     * a => 0
+     * b => 0
+     * ...
+     * e => 1
+     * ...
+     * l => 2
+     * m => 0
+     * n => 0
+     * o => 1
+     *
+     * @param s - this is the string that needs to be analyzed
+     */
+    public static void letterFrequency(String s) {
+        // your implementation goes here
+    }
+
+    /**
+     * Print the # of consonants, digits and vowels in the string
+     * e.g. "H3ll0 World" should print
+     * Consonants: 7
+     * Vowels: 1
+     * Digits: 2
+     *
+     * @param s - this is the string to be analyzed
+     */
+    public static void consonantsDigitsAndVowels(String s) {
+        // your implementation here
+    }
+    /**
+     * This function will break up the given string into words and print one line per word in the string
+     * @param s - this is the string to break up
+     */
+    public static void words(String s) {
+        // your implementation goes here
+    }
+
+    /**
+     * This function will reverse the string so Hello will become olleH
+     * @param s - this is the String to reverse
+     */
+    public static void reverse(String s) {
+        //your implementation goes here
+    }
+
+
+    /**
+     * Check if the string is entirely made up of digits
+     * "123" should return true
+     * "H123" should return false
+     * "123 " should return false
+     * "12 3" should also return false
+     * @param s - this is the string to analyze
+     * @return true if the string is all digits, false otherwise
+     */
+    public static boolean isNumber(String s) {
+        return false;
+    }
+
+    /**
+     * Convert the String to a number, throw the NumberFormatException if the String is not a number.
+     * @param s - the string to convert
+     * @return the numeric value of the string
+     * @throws NumberFormatException - exception thrown when the String is not a number
+     */
+    public static int toNumber(String s) throws NumberFormatException
+    {
+        return 0;
+    }
+}
