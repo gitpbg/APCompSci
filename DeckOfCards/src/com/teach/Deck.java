9b62ea2a99aa5f4c8d874cba80cb62743afcb093
gharpure <prasad@gharpure.com>
2017-02-03 18:54:40 -0800

    Added Deck Of Cards

---
+++ DeckOfCards/src/com/teach/Deck.java
@@ -0,0 +1,23 @@
+package com.teach;
+
+/**
+ * Created by gharpure on 2/3/17.
+ */
+public class Deck {
+    // your goal is to define a deck of cards
+
+    void resetDeck() {
+        // provide a function to reset the deck
+    }
+
+    void shuffleDeck() {
+        //
+    }
+
+    int nextCard() {
+        return 0;
+    }
+
+    void printCard(int i) {
+    }
+}
---
+++ DeckOfCards/src/com/teach/Main.java
@@ -0,0 +1,7 @@
+package com.teach;
+
+public class Main {
+
+    public static void main(String[] args) {
+    }
+}
